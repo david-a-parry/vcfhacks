bb8b23fcd887592461caaeb003d2223f42f9d963
David A. Parry <d.a.parry@leeds.ac.uk>
2014-04-28 12:17:41 +0100

    better handling of differently represented alleles. Fixed bug with allele checking.

--- annotateSnps.pl
+++ annotateSnps.pl
@@ -133,7 +133,7 @@ if ($opts{build} || $freq){
     print STDERR "Filtering variants on following criteria:\n";
     print STDERR ">in dbSNP$opts{build} or previous\n" if defined $opts{build} && $opts{build} ;
     print STDERR ">with a minor allele frequency equal to or greater than $freq (" . $freq * 100 . " %)\n" if defined $freq && $freq ;
-    print STDERR ">without  \"pathogenic\" or \"probably pathogenic\" annotations\n" if defined $opts{pathogenic} && $opts{pathogenic} ;
+    print STDERR ">without \"pathogenic\" or \"probably pathogenic\" annotations\n" if defined $opts{pathogenic} && $opts{pathogenic} ;
 }elsif ($opts{pathogenic}){
     if ($KNOWN){
         print STDERR "Annotating output and printing variants with \"pathogenic\" or \"probably pathogenic\" annotations to $opts{known_snps}\n";
@@ -152,50 +152,64 @@ if (defined $opts{Progress} and $total_vcf){
 
 VAR: while (my $line = $vcf_obj->readLine){
     $n++;
-    my $snp_meets_filter_criteria = 0; 
-    my $snp_should_not_be_filtered = 0; #flag in case pathogenic flag is set or somethinga and we don't want to filter this snp no matter what
+    my $line_should_not_be_filtered = 0; #flag in case pathogenic flag is set or somethinga and we don't want to filter this snp no matter what
     my $is_known_snp = 0;
     my $replaced_already = 0; #use this flag to allow appending of multiple IDs found in our reference file even if --replace option is in place.
     if (defined $opts{Progress}){
            $next_update = $progressbar->update($n) if $n >= $next_update;
     }
-    my $qual = $vcf_obj->getVariantField('QUAL');
-    my $chrom = $vcf_obj->getVariantField('CHROM');
-    my $pos = $vcf_obj->getVariantField('POS');
-    my $ref = $vcf_obj->getVariantField('REF');
-    foreach my $snp_obj (@snp_objs){
-        if ($snp_obj->searchForPosition(chrom=> $chrom, pos => $pos)){
-            while (my $snp_line = $snp_obj->readPosition()){#need to edit the below so we traverse through potentially multiple SNPs and still append/filter as necessarry
-                #check whether the snp line(s) match our variant
-                if (checkSnpMatches($vcf_obj, $snp_obj, $opts{samples})){
-                    $is_known_snp++;
-                    #replace or append to ID field
-                    if ($vcf_obj->getVariantField('ID') eq '.' || ($opts{replace} && not $replaced_already)){
-                        $replaced_already++;
-                        $line = $vcf_obj->replaceVariantField('ID', $snp_obj->getVariantField('ID'));
-                    }else{
-                        my $id = $vcf_obj->getVariantField('ID');
-                        my @split_id = split(";", $id);
-                    #check it's not already correctly annotated
-                        foreach my $sid ($snp_obj->getVariantField('ID')){
-                            if (not grep {/^$sid$/i} @split_id){
-                                $id .= ";". $sid; 
+
+    #process each allele separately in case we have MNVs/deletions that need to be simplified
+    #
+    my %min_vars = $vcf_obj->minimizeAlleles();
+    my %sample_alleles = ();
+    if (@samples){
+        %sample_alleles = map{$_ => undef}  $vcf_obj->getSampleCall(multiple => \@samples, return_alleles_only => 1);
+    }
+    foreach my $allele (keys %min_vars){
+        $min_vars{$allele}->{filter_snp} = 0;
+        if (@samples){
+            #doesn't exist in any sample...
+            next if not exists $sample_alleles{$allele};
+        }
+        foreach my $snp_obj (@snp_objs){
+            if ($snp_obj->searchForPosition(chrom => $min_vars{$allele}->{CHROM}, pos => $min_vars{$allele}->{POS})){
+                while (my $snp_line = $snp_obj->readPosition()){
+                    #check whether the snp line(s) match our variant
+                    if (checkSnpMatches($min_vars{$allele}, $snp_obj)){
+                        $is_known_snp++;
+                        #replace or append to ID field
+                        if ($vcf_obj->getVariantField('ID') eq '.' || ($opts{replace} && not $replaced_already)){
+                            $replaced_already++;
+                            $line = $vcf_obj->replaceVariantField('ID', $snp_obj->getVariantField('ID'));
+                        }else{
+                            my $id = $vcf_obj->getVariantField('ID');
+                            my @split_id = split(";", $id);
+                        #check it's not already correctly annotated
+                            foreach my $sid ($snp_obj->getVariantField('ID')){
+                                if (not grep {/^$sid$/i} @split_id){
+                                    $id .= ";". $sid; 
+                                }
                             }
+                            $line = $vcf_obj->replaceVariantField('ID', $id);
                         }
-                        $line = $vcf_obj->replaceVariantField('ID', $id);
-                    }
-                    #perform filtering if fiters are set
-                    if ($opts{build} || $opts{pathogenic} || $freq){
-                        my ($filter_snp, $dont_filter)  = evaluate_snp($opts{build}, $opts{pathogenic}, $freq, $vcf_obj, $snp_obj, $opts{samples});
-                        $snp_meets_filter_criteria += $filter_snp;
-                        $snp_should_not_be_filtered += $dont_filter;
+                        #perform filtering if fiters are set
+                        if ($opts{build} || $opts{pathogenic} || $freq){
+                            my ($filter_snp, $dont_filter)  = evaluate_snp($opts{build}, $opts{pathogenic}, $freq, $snp_obj);
+                            $min_vars{$allele}->{filter_snp} += $filter_snp;
+                            $line_should_not_be_filtered += $dont_filter;
+                        }
                     }
                 }
             }
         }
     }
+    my $filter_count = 0;
+    foreach my $allele (keys %min_vars){
+        $filter_count++ if ($min_vars{$allele}->{filter_snp});
+    }
     if ($opts{build} || $opts{pathogenic} || $freq){#if using filtering only put filtered in $KNOWN
-        if ( $snp_should_not_be_filtered ){ #at the moment this means it's pathogenic flagged and $opts{pathogenic} is set 
+        if ( $line_should_not_be_filtered ){ #at the moment this means it's pathogenic flagged and $opts{pathogenic} is set 
                                             #so if --build or --freq are set print to $OUT (keep), if not print to $KNOWN and $OUT
              $kept++;
              print $OUT "$line\n";
@@ -203,7 +217,7 @@ VAR: while (my $line = $vcf_obj->readLine){
                 print $KNOWN "$line\n" if $KNOWN;
                 $pathogenic_snps++;
             }
-        }elsif( $snp_meets_filter_criteria ){ #filter 
+        }elsif( $filter_count == keys(%min_vars) ){ #filter if all alleles meet criteria
             $filtered++;
             print $KNOWN "$line\n" if $KNOWN;
         }else{ # don't filter
@@ -239,15 +253,11 @@ sub evaluate_snp{
     #returns two values - first is 1 if snp should be filtered and 0 if not, 
     #the second is 1 if shouldn't be filtered under any circumstance (at the moment only if
     #pathogenic flag is set and snp has a SCS or CLNSIG value of 4 or 5
-    my ($build, $path, $freq, $vcf_obj, $snp_obj, $samp_ar) = @_;
-    my $matches = checkSnpMatches($vcf_obj, $snp_obj, $samp_ar);
-    if (not $matches){
-        return (0, 0);
-    }
+    my ($build, $path, $freq, $snp_obj) = @_;
     my $snp_info = $snp_obj->getVariantField("INFO");
     if ($path){
         if ($snp_info =~ /SCS=([\d])/){
-#SCS=4 indicates probable-pathogenic, SCS=5 indicates pathogenic, print regardless of freq or build if --pathogenic option in use
+         #SCS=4 indicates probable-pathogenic, SCS=5 indicates pathogenic, print regardless of freq or build if --pathogenic option in use
             if ($1 == 4 or $1 ==  5){
                 return (0, 1);
             }
@@ -303,35 +313,19 @@ sub evaluate_snp{
 }
 #################################################
 sub checkSnpMatches{
-    my ($obj, $snp, $samples_ref) = @_;
-
-    my $chrom = $obj->getVariantField("CHROM");
-    my $pos = $obj->getVariantField("POS");
-    my $snp_chrom = $snp->getVariantField("CHROM");
-    my $snp_pos = $snp->getVariantField("POS");
-    my $snp_ref = $snp->getVariantField("REF");
-    #my $ref = $obj->getVariantField("REF");
-    #my $ref = $obj->getVariantField("REF");
-    #my @alts = split(",", $obj->getVariantField("ALT"));
-    $chrom =~ s/^chr//;
-    $snp_chrom =~ s/^chr//;
-    return 0 if $chrom ne $snp_chrom;
-    return 0 if $pos ne $snp_pos;
-    my @snp_alleles = $snp->readAlleles();    
-    my @alleles = ();
-    if (@$samples_ref){
-        @alleles = $obj->getSampleActualGenotypes(multiple => $samples_ref, return_alleles_only => 1);
-    }else{
-        @alleles = $obj->readAlleles();    
+    my ($min_allele, $snp) = @_;
+    my $matches = 0;
+    my %snp_min = $snp->minimizeAlleles();
+    foreach my $snp_allele (keys %snp_min){
+        $min_allele->{CHROM} =~ s/^chr//;
+        $snp_min{$snp_allele}->{CHROM} =~ s/^chr//;
+        next if $min_allele->{CHROM} ne $snp_min{$snp_allele}->{CHROM};
+        next if $min_allele->{POS} ne $snp_min{$snp_allele}->{POS};
+        next if $min_allele->{REF} ne $snp_min{$snp_allele}->{REF};
+        next if $min_allele->{ALT} ne $snp_min{$snp_allele}->{ALT};
+        $matches++;
     }
-    #if none of our samples has a variant here then 
-    #we might as well skip if snp meets our filters
-    #so let's say it matches as default behaviour
-    return 1 if not @alleles;    
-    foreach my $allele (@alleles){
-        return 0 if not grep { /^$allele$/ } @snp_alleles;
-    }
-    return 1;
+    return $matches;
 }
         
 =head1 NAME
@@ -374,7 +368,7 @@ Use this option to replace the ID field with IDs from your SNP reference VCF fil
 
 =item B<--samples>
 
-One or more samples to check variants for.  Default is to check all variants specified by the ALT field.
+One or more samples to check variants for.  Default is to check all variants specified by the ALT field. If specified, SNPs will not be filtered unless variant is present in at least one of the samples specified by this argument.
 
 =item B<-b    --build>
 
