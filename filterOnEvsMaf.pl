07c2c536ad6ce7c8f1d060f214f6524ad658a4ba
David A. Parry <d.a.parry@leeds.ac.uk>
2014-04-28 12:31:51 +0100

    better handling of differently represented alleles. Fixed bug with allele checking.

--- filterOnEvsMaf.pl
+++ filterOnEvsMaf.pl
@@ -51,7 +51,6 @@ my $kept = 0; #variants not filtered
 my $filtered = 0; #variants filtered
 my $pathogenic_snps = 0;
 my $found = 0; #variants that match a known SNP
-my $lines = 0; 
 my $time = strftime("%H:%M:%S", localtime);
 print STDERR "[$time] Initializing input VCF\n";
 my $vcf_obj = ParseVCF->new( file=> $opts{input});
@@ -132,48 +131,58 @@ VAR: while (my $line = $vcf_obj->readLine){
     if (defined $opts{Progress}){
            $next_update = $progressbar->update($n) if $n >= $next_update;
     }
-    my $qual = $vcf_obj->getVariantField('QUAL');
-    my $chrom = $vcf_obj->getVariantField('CHROM');
-    my $pos = $vcf_obj->getVariantField('POS');
-    my $ref = $vcf_obj->getVariantField('REF');
-    foreach my $snp_obj (@snp_objs){
-        if ($snp_obj->searchForPosition(chrom=> $chrom, pos => $pos)){
-            while (my $snp_line = $snp_obj->readPosition()){#need to edit the below so we traverse through potentially multiple SNPs and still append/filter as necessarry
-                #check whether the snp line(s) match our variant
-                if (checkSnpMatches($vcf_obj, $snp_obj, $opts{samples})){
-                    $is_known_snp++;
-                    #perform filtering if fiters are set
-                    if ($freq){
-                        my $snp_info = $snp_obj->getVariantField("INFO");
-                        my @info = split(';', $snp_info);
-                        foreach my $info (@info){
-                            if ($info =~ /^MAF=(.*)/){
-                                my @mafs = split(',', $1);
-                                foreach my $maf (@mafs){
-                                    if ($maf >= $freq){
-                                        $filtered++;
-                                        $found++ ;
-                                        next VAR;
+    my %min_vars = $vcf_obj->minimizeAlleles();
+    my %sample_alleles = ();
+    if (@samples){
+        %sample_alleles = map{$_ => undef}  $vcf_obj->getSampleCall(multiple => \@samples, return_alleles_only => 1);
+    }
+ALLELE:    foreach my $allele (keys %min_vars){
+        $min_vars{$allele}->{filter_snp} = 0;
+        if (@samples){
+            #doesn't exist in any sample...
+            next if not exists $sample_alleles{$allele};
+        }
+        foreach my $snp_obj (@snp_objs){
+            if ($snp_obj->searchForPosition(chrom => $min_vars{$allele}->{CHROM}, pos => $min_vars{$allele}->{POS})){
+                while (my $snp_line = $snp_obj->readPosition()){#need to edit the below so we traverse through potentially multiple SNPs and still append/filter as necessarry
+                    #check whether the snp line(s) match our variant
+                    if (checkSnpMatches($min_vars{$allele}, $snp_obj)){
+                        $is_known_snp++;
+                        #perform filtering if fiters are set
+                        if ($freq){
+                            my $snp_info = $snp_obj->getVariantField("INFO");
+                            my @info = split(';', $snp_info);
+                            foreach my $info (@info){
+                                if ($info =~ /^MAF=(.*)/){
+                                    my @mafs = split(',', $1);
+                                    foreach my $maf (@mafs){
+                                        if ($maf >= $freq){
+                                            $min_vars{$allele}->{filter_snp}++ ;
+                                            next ALLELE;
+                                        }
                                     }
                                 }
                             }
+                        }else{#default behaviour is to filter everything that matches
+                            $min_vars{$allele}->{filter_snp}++ ;
+                            next ALLELE;
                         }
-                    }else{#default behaviour is to filter everything that matches
-                        $filtered++;
-                        $found++ ;
-                        next VAR ;
-
                     }
                 }
             }
         }
-    }#not found
-    print $OUT "$line\n";
-    $kept++;
-    $found++ if $is_known_snp;
-    if (defined $opts{Progress}){
-        $next_update = $progressbar->update($lines) if $lines >= $next_update;
     }
+    $found++ if $is_known_snp;
+    foreach my $allele (keys %min_vars){
+        if (not $min_vars{$allele}->{filter_snp}){
+            #print if ANY of the alleles haven't met 
+            #our filter criteria
+            print $OUT "$line\n";
+            $kept++;
+            next VAR;
+        }
+    }
+    $filtered++;
 }
 if (defined $opts{Progress}){
         $progressbar->update($total_vcf) if $total_vcf >= $next_update;
@@ -186,104 +195,25 @@ print STDERR "$filtered variants filtered, $kept variants retained.\n";
 
 ################################################
 #####################SUBS#######################
-sub evaluate_snp{
-    #returns two values - first is 1 if snp should be filtered and 0 if not, 
-    #the second is 1 if shouldn't be filtered under any circumstance (at the moment only if
-    #pathogenic flag is set and snp has a SCS or CLNSIG value of 4 or 5
-    my ($build, $path, $freq, $vcf_obj, $snp_obj, $samp_ar) = @_;
-    my $matches = checkSnpMatches($vcf_obj, $snp_obj, $samp_ar);
-    if (not $matches){
-        return (0, 0);
-    }
-    my $snp_info = $snp_obj->getVariantField("INFO");
-    if ($path){
-        if ($snp_info =~ /SCS=([\d])/){
-#SCS=4 indicates probable-pathogenic, SCS=5 indicates pathogenic, print regardless of freq or build if --pathogenic option in use
-            if ($1 == 4 or $1 ==  5){
-                return (0, 1);
-            }
-        }else{
-            #die "No SCS field in snp line $snp_line" if $strict; #the SCS field appears to be dropped from the GATK bundle now
-            #print STDERR "Warning, no SCS field in snp line $snp_line\n" unless $quiet;
-        }
-        if ($snp_info =~ /CLNSIG=([\d])/){
-            if ($1 == 4 or $1 ==  5){
-                return (0, 1);
-            }
-        }
-    }
-    if ($build){
-        if ($snp_info =~ /dbSNPBuildID=([\d]+)/){
-            if ($build >= $1){
-                return (1, 0);
-            }
-        }else{
-            die "No dbSNPBuildID field in snp line " .$snp_obj->get_currentLine ."\n"  if $strict;
-            print STDERR "Warning, no dbSNPBuildID field in snp line " . $snp_obj->get_currentLine  . "\n" unless $quiet;
-        }
-    }
-    if ($freq){
-        my @info = split(/\;/, $snp_info);
-        if ($freq <= 0.05){
-            #G5 = minor allele freq > 5 % in at least 1 pop
-            #G5A = minor allele freq > 5 % in all pops
-            if (grep{/^(G5|G5A)/}@info){ 
-                return (1, 0);
-            }
-        }
-        my (@af) = grep {/^AF=[\d]\.[\d]+/} @info;
-        if (@af){
-            foreach my $af (@af){
-                $af =~ s/AF=//;
-                if ($freq <= $af){
-                    return (1, 0);
-                }
-            }
-        }
-        my (@gmaf) = grep {/^GMAF=[\d]\.[\d]+/} @info;
-        if (@gmaf){
-            foreach my $gmaf (@gmaf){
-                $gmaf =~ s/GMAF=//;
-                if ($freq <= $gmaf){
-                    return (1, 0);
-                }
-            }
-        }
-    }
-    return (0, 0);
-}
-#################################################
-sub checkSnpMatches{
-    my ($obj, $snp, $samples_ref) = @_;
+################################################
 
-    my $chrom = $obj->getVariantField("CHROM");
-    my $pos = $obj->getVariantField("POS");
-    my $snp_chrom = $snp->getVariantField("CHROM");
-    my $snp_pos = $snp->getVariantField("POS");
-    my $snp_ref = $snp->getVariantField("REF");
-    #my $ref = $obj->getVariantField("REF");
-    #my $ref = $obj->getVariantField("REF");
-    #my @alts = split(",", $obj->getVariantField("ALT"));
-    $chrom =~ s/^chr//;
-    $snp_chrom =~ s/^chr//;
-    return 0 if $chrom ne $snp_chrom;
-    return 0 if $pos ne $snp_pos;
-    my @snp_alleles = $snp->readAlleles();    
-    my @alleles = ();
-    if (@$samples_ref){
-        @alleles = $obj->getSampleActualGenotypes(multiple => $samples_ref, return_alleles_only => 1);
-    }else{
-        @alleles = $obj->readAlleles();    
+sub checkSnpMatches{
+    my ($min_allele, $snp) = @_;
+    my $matches = 0;
+    my %snp_min = $snp->minimizeAlleles();
+    foreach my $snp_allele (keys %snp_min){
+        $min_allele->{CHROM} =~ s/^chr//;
+        $snp_min{$snp_allele}->{CHROM} =~ s/^chr//;
+        next if $min_allele->{CHROM} ne $snp_min{$snp_allele}->{CHROM};
+        next if $min_allele->{POS} ne $snp_min{$snp_allele}->{POS};
+        next if $min_allele->{REF} ne $snp_min{$snp_allele}->{REF};
+        next if $min_allele->{ALT} ne $snp_min{$snp_allele}->{ALT};
+        $matches++;
     }
-    #if none of our samples has a variant here then 
-    #we might as well skip if snp meets our filters
-    #so let's say it matches as default behaviour
-    return 1 if not @alleles;    
-    foreach my $allele (@alleles){
-        return 0 if not grep { /^$allele$/ } @snp_alleles;
-    }
-    return 1;
-}        
+    return $matches;
+}
+        
+################################################
 
 =head1 NAME
 
@@ -320,7 +250,7 @@ One or more ESP VCF files to use.
 
 =item B<--samples>
 
-One or more samples to check variants for.  Default is to check all variants specified by the ALT field.
+One or more samples to check variants for.  Default is to check all variants specified by the ALT field. If specified, variants will not be filtered unless variant is present in at least one of the samples specified by this argument.
 
 =item B<-f    --freq>
 
