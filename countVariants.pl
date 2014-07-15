a01e47cfc70eb5d68f279fbc3f50034fb2cb8cb2
David A. Parry <d.a.parry@leeds.ac.uk>
2014-07-15 11:15:56 +0100

    use VcfReader.pm

--- countVariants.pl
+++ countVariants.pl
@@ -3,13 +3,12 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin";
-use ParseVCF;
+use VcfReader;
 
 die "Usage: countVariants.pl input1.vcf input2.vcf ...\n" if @ARGV <  1;
 my $total = 0;
 while (my $vcf = shift){ 
-    my $obj = ParseVCF->new(file => $vcf);
-    my $variants = $obj->countLines() ;
+    my $variants = VcfReader::countVariants($vcf) ;
     print "$vcf:\t$variants variants\n";
     $total += $variants;
 }
