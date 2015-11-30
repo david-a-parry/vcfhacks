05df20384f83a6420bdf84f501114a70e983082e
David A. Parry <gantzgraf@github.com>
2015-11-30 21:36:57 +0000

    correct short opt for --progress in messages

--- findBiallelic.pl
+++ findBiallelic.pl
@@ -233,10 +233,10 @@ my $total_vars  = 0;
 my $var_count   = 0;
 if ($opts{b}) {
     if ( $opts{i} eq "-" ) {
-        informUser("Can't use --progress option when input is from STDIN\n");
+        informUser("Can't use -b/--progress option when input is from STDIN\n");
     }else{
         my $msg = <<EOT
-WARNING: -p/--progress option requires installation of the perl module 'Term::ProgressBar'.
+WARNING: -b/--progress option requires installation of the perl module 'Term::ProgressBar'.
 WARNING: 'Term::ProgressBar' module was not found. No progress will be shown.
 EOT
 ;
@@ -1929,7 +1929,7 @@ University of Edinburgh
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2012, 2013, 2014, 2015, David A. Parry
+Copyright 2015, David A. Parry
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
