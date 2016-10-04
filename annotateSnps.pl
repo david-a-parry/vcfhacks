77e62fdf156f162edb6657722820d8b8e1a0f3bb
David A. Parry <david.parry@igmm.ed.ac.uk>
2016-10-04 18:08:24 +0100

    do not use A for info fields that are flags

--- annotateSnps.pl
+++ annotateSnps.pl
@@ -994,10 +994,14 @@ DBSNP:      foreach my $d (@dbsnp){
                 if ( $dbsnp_to_info{$d}->{$field} ){
                     print STDERR "[$time] INFO - $field field found in $d...\n";
                     (my $desc = $dbsnp_to_info{$d}->{$field}->{Description}) =~ s/\"//g;
+                    my $n = 'A';
+                    if ($dbsnp_to_info{$d}->{$field}->{Type} eq 'Flag'){
+                        $n = '.';
+                    }
                     my %d_info = 
                     (
                         ID          => "AS_$field",
-                        Number      => "A",
+                        Number      => $n,
                         Type        => $dbsnp_to_info{$d}->{$field}->{Type},
                         Description => "This annotation has been altered by ".
                                        "annotateSnps.pl to report consequences".
